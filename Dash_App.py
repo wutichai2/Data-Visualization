2934b92e572b471124d04271e2a9297e478f1055
wutichai2 <wuttichai.j@grouplease.co.h>
2019-06-26 14:04:05 +0700

    Create test import seaborn,matplotlib

--- Dash_App.py
+++ Dash_App.py
@@ -3,7 +3,19 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
-app.layout = html.Div('test datavis')
+app.layout = html.Div(children=[
+    html.H1('test datavis'),
+    dcc.Graph(id='test',
+              figure={
+                  'data' : [
+                      {'x' :[1,2,3,4,5], 'y':[5,6,7,8,9,1], 'type':'line','name':'wut'},
+                      {'x' :[1,2,3,4,5], 'y':[8,3,6,1,2,5], 'type':'bar','name':'cars'}
+                  ],
+                  'layout': {
+                      'title':'Basic Dash Example'
+                  }
+              })
+        ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
